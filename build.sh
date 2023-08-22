b9f321638e614246874f899e94eeb5429e794c53
Medona Jugi P <108681859+Medona-Jugi-P@users.noreply.github.com>
2023-08-22 23:17:17 +0530

    Initial commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+
+# Upgrade pip to the latest version
+pip install --upgrade pip
+
+# Install Python dependencies from requirements.txt
+pip install -r requirements.txt
